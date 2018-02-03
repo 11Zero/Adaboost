f383610fe83f3cc5c6c8e4f676141fa4334c3088
11Zero <874612467@qq.com>
2018-02-03 15:09:39 +0800

    svm弱分类器输入参数调整为2个，效果还不行

--- run.py
+++ run.py
@@ -28,23 +28,29 @@ def get_totalaccuracy(predict_train_rsl,train_rsl,predict_test_rsl,test_rsl,):
         if predict_test_rsl[i] == test_rsl[i]:
             shooted += 1
 
-    return shooted/(len(predict_train_rsl)+len(predict_test_rsl))
+    return shooted/(np.size(predict_train_rsl)+np.size(predict_test_rsl))
 
 def get_weight(singal_svm,x_test,y_test,_D):
-
-    max = x_test.max()
-    min = x_test.min()
-    x_test_normal = (x_test - min) / (max - min)
-    targeted_rate = singal_svm.score(x_test_normal, y_test)
+    max = np.zeros((1, np.size(x_test[0, :])))
+    min = np.zeros((1, np.size(x_test[0, :])))
+    test_x_normal = np.mat(np.zeros(np.shape(x_test)))
+    for i in range(np.size(x_test[0, :])):
+        max[0, i] = x_test[:, i].max()
+        min[0, i] = x_test[:, i].min()
+        if max[0, i] == min[0, i]:
+            test_x_normal[:, i] = np.mat(np.ones(np.shape(x_test[:, i])))
+        else:
+            test_x_normal[:, i] = (x_test[:, i] - min[0, i]) / (max[0, i] - min[0, i])
+    targeted_rate = singal_svm.score(test_x_normal, y_test)
     err_weight = 0
-    y_hat_test_predict = singal_svm.predict(x_test_normal)
-    for i in range(len(y_hat_test_predict)):
+    y_hat_test_predict = singal_svm.predict(test_x_normal)
+    for i in range(np.size(y_hat_test_predict)):
         if y_hat_test_predict[i] != y_test[i]:
             err_weight += _D[i]
     if err_weight == 0:
         err_weight = 1e-16
     alpha = 1/2*np.log((1-err_weight)/err_weight)
-    for i in range(len(y_hat_test_predict)):
+    for i in range(np.size(y_hat_test_predict)):
         if y_hat_test_predict[i] != y_test[i]:
             _D[i] = _D[i]/2/err_weight
         else:
@@ -53,14 +59,26 @@ def get_weight(singal_svm,x_test,y_test,_D):
 
 def single_svm_train(train_x,train_y,test_x,test_y):
 
-    clf = svm.SVC(C=0.5, kernel='rbf', gamma=40, decision_function_shape='ovr')
-    max = train_x.max()
-    min = train_x.min()
-    train_x_normal = (train_x-min)/(max - min)
+    clf = svm.SVC(C=0.5, kernel='linear', gamma=40, decision_function_shape='ovr')
+    max = np.zeros((1,np.size(train_x[0,:])))
+    min = np.zeros((1,np.size(train_x[0,:])))
+    train_x_normal = np.mat(np.zeros(np.shape(train_x)))
+    for i in range(np.size(train_x[0,:])):
+        max[0,i] = train_x[:,i].max()
+        min[0, i] = train_x[:, i].min()
+        if max[0, i] == min[0, i]:
+            train_x_normal[:, i] = np.mat(np.ones(np.shape(train_x_normal[:, i])))
+        else:
+            train_x_normal[:, i] = (train_x[:, i] - min[0, i]) / (max[0, i] - min[0, i])
     clf.fit(train_x_normal, train_y)
-    max = test_x.max()
-    min = test_x.min()
-    test_x_normal = (test_x - min) / (max - min)
+    test_x_normal = np.mat(np.zeros(np.shape(test_x)))
+    for i in range(np.size(test_x[0, :])):
+        max[0, i] = test_x[:, i].max()
+        min[0, i] = test_x[:, i].min()
+        if max[0, i] == min[0, i]:
+            test_x_normal[:, i] = np.mat(np.ones(np.shape(test_x_normal[:, i])))
+        else:
+            test_x_normal[:, i] = (x_test[:, i] - min[0, i]) / (max[0, i] - min[0, i])
     targeted_rate = clf.score(test_x_normal, test_y)
     return targeted_rate,clf
 
@@ -78,7 +96,7 @@ def single_svm_train(train_x,train_y,test_x,test_y):
 # exit()
 train_name = 'data1.json'
 test_name = 'data2.json'
-train_size = 0.8
+train_size = 0.7
 logger = logging.getLogger("recording")
 logger.setLevel(logging.DEBUG)
 # 建立一个filehandler来把日志记录在文件里，级别为debug以上
@@ -108,11 +126,10 @@ x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(all_
 test_data_rows = len(y_test)
 train_data_rows = len(y_train)
 
-score = np.zeros((data_cols-1,1))
+score = np.zeros((data_cols-2,1))
 svm_sets = np.zeros((len(score),1)).tolist()
-for i in range(data_cols-1):
-    x = x_train[:,i]
-    result = single_svm_train(x_train[:,i],y_train,x_test[:,i],y_test)
+for i in range(data_cols-2):
+    result = single_svm_train(x_train[:,i:i+2],y_train,x_test[:,i:i+2],y_test)
     score[i] = result[0]
     svm_sets[i] = result[1]
     print("因素"+str(i+1)+"样本集准确率为："+str(result[0]))
@@ -127,14 +144,24 @@ D = np.ones((test_data_rows,1))*1/test_data_rows
 svm_weights = np.zeros((len(score),1))
 final_result = np.zeros((1,test_data_rows))
 for i in range(len(score_order)):
-    x = x_test[:,int(score_order[i,0])]
+    x = x_test[:,int(score_order[i,0]):int(score_order[i,0])+2]
     svm = svm_sets[int(score_order[i,0])]
     print("D = " + str(D.T))
     result = get_weight(svm,x,y_test,D)
     D = result[1]
-    max = x.max()
-    min = x.min()
-    x_normal = (x - min) / (max - min)
+    x_normal = np.mat(np.zeros(np.shape(x)))
+    max = np.zeros((1, np.size(x[0, :])))
+    min = np.zeros((1, np.size(x[0, :])))
+    for j in range(np.size(x[0, :])):
+        max[0, j] = x[:, j].max()
+        min[0, j] = x[:, j].min()
+        if max[0, j] == min[0, j]:
+            x_normal[:, j] = np.mat(np.ones(np.shape(x_normal[:, j])))
+        else:
+            x_normal[:, j] = (x[:, j] - min[0, j]) / (max[0, j] - min[0, j])
+    # max = x.max()
+    # min = x.min()
+    # x_normal = (x - min) / (max - min)
     final_result = final_result+result[2]*svm.predict(x_normal)
     svm_weights[int(score_order[i,0])] = result[2]
     err = 0
@@ -160,7 +187,7 @@ for i in range(len(final_result)):
     elif final_result[i]<0:
         final_result[i] = -1
     if final_result[i] != y_test[i]:
-        err+=1
+        err = err + 1
 print(final_result)
 print(y_test.T.tolist()[0])
 print("强分类器准确率为："+str(1-err/len(final_result)))
@@ -173,9 +200,18 @@ data_rows = len(test_data)
 data_cols = len(test_data[0])
 y = np.mat(test_data)[:,-1]
 final_result = np.zeros((1,data_rows))
-
 for i in range(len(svm_weights)):
-    x = np.mat(test_data)[:,i]
+    x = np.mat(test_data)[:,i:i+2]
+    x_normal = np.mat(np.zeros(np.shape(x)))
+    max = np.zeros((1, np.size(x[0, :])))
+    min = np.zeros((1, np.size(x[0, :])))
+    for j in range(np.size(x[0, :])):
+        max[0, j] = x[:, j].max()
+        min[0, j] = x[:, j].min()
+        if max[0, j] == min[0, j]:
+            x_normal[:, j] = np.mat(np.ones(np.shape(x_normal[:, j])))
+        else:
+            x_normal[:, j] = (x[:, j] - min[0, j]) / (max[0, j] - min[0, j])
     single_svm = svm_sets[i]
     forecast = single_svm.predict(x)
     final_result = final_result+svm_weights[i]*forecast
